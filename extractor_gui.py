8dcaf894ebb03d26c8ce76c9c5eacd9c4e8bb39a
meru3m <gustokomagprogram@gmail.com>
2024-07-21 21:39:55 +0800

    Arranged widgets alignments

--- extractor_gui.py
+++ extractor_gui.py
@@ -2,12 +2,24 @@ import FreeSimpleGUI as sg
 from zip_extractor import extract_archive
 
 sg.theme("Black")
-
-layout = [[sg.Text("Select Archive:"), sg.InputText(key="file_input"), sg.FileBrowse("Choose", key="file")],
-          [sg.Text("Select Directory:"), sg.InputText(key="file_dest"), sg.FolderBrowse("Choose", key="folder")],
-          [sg.Button("Extract"), sg.Button("Exit"), sg.Text(key="output", text_color="Green")]]
-
-window = sg.Window("Archive Extractor", layout)
+ 
+label1 = sg.Text("Select archive:")
+input1 = sg.Input()
+choose_button1 = sg.FileBrowse("Choose", key="archive")
+ 
+label2 = sg.Text("Select destination directory:")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
+ 
+extract_button = sg.Button("Extract")
+output_label = sg.Text(key="output", text_color="green")
+ 
+col1 = sg.Column([[label1], [label2]])
+col2 = sg.Column([[input1], [input2]])
+col3 = sg.Column([[choose_button1], [choose_button2]])
+ 
+window = sg.Window("Archive Extractor",
+                   layout=[[col1, col2, col3], [extract_button]])
 
 while True:
     event, values = window.read()
